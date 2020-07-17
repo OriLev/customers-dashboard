a3610a69b6b71805e0fb85a71d4f38444564d947
Ori Levitan <orilevitan2@gmail.com>
2020-07-17 07:16:41 +0300

    refactor styling for mobile compatability

--- src/components/CustomerDetails/CustomerDetails.js
+++ src/components/CustomerDetails/CustomerDetails.js
@@ -36,9 +36,15 @@ export const CustomerDetails = ({
       <div className="customerDetails">
         <h2 className="customerName">
           {name.first + ' ' + name.last} &nbsp;
-          <span className={`userStatus ${isActive ? 'active' : 'inactive'}`}>({isActive ? 'Active user' : 'Inactive user'})</span>
+          <span className={`userStatus ${isActive ? 'active' : 'inactive'}`}>
+            ({isActive ? 'Active user' : 'Inactive user'})
+          </span>
         </h2>
-        <img className="customerImage" src={picture} alt={name.first + ' ' + name.last} />
+        <img
+          className="customerImage"
+          src={picture}
+          alt={name.first + ' ' + name.last}
+        />
         <Detail detailName="Balance" detail={balance} />
         <Detail detailName="Age" detail={age} />
         <Detail detailName="Eye Color" detail={eyeColor} />
@@ -47,10 +53,10 @@ export const CustomerDetails = ({
         <Detail detailName="Phone" detail={phone} />
         <Detail detailName="Address" detail={address} />
         <Detail detailName="About" detail={about} />
+        <button className="activationToggle" onClick={() => toggleActive()}>
+          {!customer.isActive ? 'Activate user' : 'Deactivate user'}
+        </button>
       </div>
-      <button className="activationToggle" onClick={() => toggleActive()}>
-        {!customer.isActive ? 'Activate user' : 'Deactivate user'}
-      </button>
     </>
   );
 };
