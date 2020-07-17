2010d579541049c2230af7d1515fed8dd458e7dc
Ori Levitan <orilevitan2@gmail.com>
2020-07-17 07:04:00 +0300

    Updates

---
+++ precache-manifest.2d5eac10e326c0f4f79f85cd56b10596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6433e4f325c774fe253da123ba7662d1",
+    "url": "/customers-dashboard/index.html"
+  },
+  {
+    "revision": "d2318e0e1dc43a6aff2a",
+    "url": "/customers-dashboard/static/css/main.022e3e71.chunk.css"
+  },
+  {
+    "revision": "e9b5cf9db5c3fc6781e4",
+    "url": "/customers-dashboard/static/js/2.e170c848.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/customers-dashboard/static/js/2.e170c848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2318e0e1dc43a6aff2a",
+    "url": "/customers-dashboard/static/js/main.7e44c28d.chunk.js"
+  },
+  {
+    "revision": "13c45f88575133da9f50",
+    "url": "/customers-dashboard/static/js/runtime-main.6f39cfb3.js"
+  }
+]);
